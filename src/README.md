802ea15b8907d56365cb0e6f5b2c493d14207d78
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-06-14 17:43:27 -0300

    hacker edition, correção no texto e readme

--- src/README.md
+++ src/README.md
@@ -1,14 +1,14 @@
-# SenhaFácil 
+## SenhaFácil 
 
-Adicionar informação sobre o processo e decisões do desenho.
-Tentei criar um sentimento de autoidentificação através de modelos mais velhos, para causar familiaridade com os usuários. O campo de textarea o usuário pode aumentar ou diminuir o tamanho, escolhendo o que seria o ideal para ele. 
+# Adicionar informação sobre o processo e decisões do desenho.
+Tentei criar um sentimento de identificação através do modelo aparentando ter mais idade, para causar familiaridade com os usuários. O campo de textarea o usuário pode aumentar ou diminuir o tamanho, para ele escolher o tamanho ideal para ele. 
 
-Quem são os principais usuário do produto?
+# Quem são os principais usuário do produto?
 Pessoas de 50 anos adiante.
 
-Quais são os objetivos do usuário em relação com o produto?
+# Quais são os objetivos do usuário em relação com o produto?
 Tornar a criação de senha menos óbvias e mais atrativa, utilizando o método de criptografar e criptografar, seja uma mensagem, o nome de um neto ou filho, etc. 
 
-Como você acredita que o produto está resolvendo os problemas do usuário?
-Geralmente as pessoas utilizam a mesma senha para bancos e plataformas, sabemos que manter o mesmo acesso não é seguro. O SenhaFácil seria um modo fácil para as pessoas criarem mais senhas seguras de modo criativo e interativo.
+# Como você acredita que o produto está resolvendo os problemas do usuário?
+Geralmente as pessoas utilizam a mesma senha para bancos e plataformas, sabemos que manter o mesmo acesso não é seguro. O SenhaFácil seria um modo fácil para as pessoas criarem senhas de modo criativo e interativo.
 
--- src/cipher.js
+++ src/cipher.js
@@ -9,44 +9,103 @@ const cipher = {
 
       for (let i = 0; i < string.length; i++) {
         let crypt = string.charCodeAt(i)
-        if (crypt >= 65 && crypt <= 90) {
-          crypt = ((crypt - 65 + offsetNumber) % 26) + 65
+
+        if (offsetNumber < 0) {
+          offsetNumber = Math.abs(offsetNumber);
+        }
+        else if (crypt >= 34 && crypt <= 63) {
+          crypt = ((crypt - 34 + offsetNumber) % 29  + 34);
+        }
+        else if (crypt >= 65 && crypt <= 90) {
+          crypt = ((crypt - 65 + offsetNumber) % 26 + 65) ;
         }
         else if (crypt >= 97 && crypt <= 122) {
-          crypt = ((crypt - 97 + offsetNumber) % 26) + 97
+          crypt = ((crypt - 97 + offsetNumber) % 26 + 97) ;
+        }
+        else if (crypt >= 123 && crypt <= 254) {
+          crypt = ((crypt - 123 + offsetNumber) % 131 + 123) ;
         }
         mensage += String.fromCharCode(crypt)
       }
       return mensage
 
     } else {
-      throw new TypeError("Deu erro")
+      throw new TypeError("Erro na codificação")
     }
   },
 
   decode: function decode(offset, string) {
-    let response = ""
+    let mensage = ""
     let offsetNumber = parseInt(offset)
 
     if (typeof offsetNumber === "number" && typeof string === "string") {
-      
+
       for (let i = 0; i < string.length; i++) {
         let decode = string.charCodeAt(i)
-        if (decode >= 65 && decode <= 90) {
+
+        if (offsetNumber < 0) {
+          offsetNumber = Math.abs(offsetNumber);
+        }
+        else if (decode >= 34 && decode <= 63) {
+          decode =  63 - ((63 - decode + offsetNumber) % 29);
+        }
+        else if (decode >= 65 && decode <= 90) {
           decode = 90 - ((90 - decode + offsetNumber) % 26);
         }
         else if (decode >= 97 && decode <= 122) {
           decode = 122 - ((122 - decode + offsetNumber) % 26);
         }
-        response += String.fromCharCode(decode)
+        else if (decode >= 123 && decode <= 254) {
+          decode = 254 - ((254 - decode + offsetNumber) % 131);
+        }
+        mensage += String.fromCharCode(decode)
       }
-      return response
+      return mensage
 
     } else {
-      throw new TypeError("Deu erro")
+      throw new TypeError("Erro na decodificação")
     }
   }
 
 };
 
 export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // switch (crypt) {
+        //   case 33:
+        //     console.log("crypt")
+        //     break;
+        //    case 'pontos':
+        //      crypt >= 91 && crypt <= 96;
+        //      break;
+        //   crypt = ((crypt - 123 + offsetNumber) % 26) + 123;
+        // }
+
+          // switch (decode) {
+        //   case 33:
+        //     console.log("teste")
+        //   case 'pontos':
+        //      decode >= 91 && decode <= 96;
+        //      break;
+        // }
