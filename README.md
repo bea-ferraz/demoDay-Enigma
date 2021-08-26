57e3ba5b1a976826f4081edb514f032141d8fe3f
Tauana Pacheco <tauanapachecopp@gmail.com>
2021-08-26 19:34:13 -0300

    Updates

---
+++ README.md
@@ -0,0 +1,14 @@
+## SenhaFácil 
+
+# Adicionar informação sobre o processo e decisões do desenho.
+Tentei criar um sentimento de identificação através do modelo aparentando ter mais idade, para causar familiaridade com os usuários. O campo de textarea o usuário pode aumentar ou diminuir o tamanho, para ele escolher o tamanho ideal para ele. 
+
+# Quem são os principais usuário do produto?
+Pessoas de 50 anos adiante.
+
+# Quais são os objetivos do usuário em relação com o produto?
+Tornar a criação de senha menos óbvias e mais atrativa, utilizando o método de criptografar e criptografar, seja uma mensagem, o nome de um neto ou filho, etc. 
+
+# Como você acredita que o produto está resolvendo os problemas do usuário?
+Geralmente as pessoas utilizam a mesma senha para bancos e plataformas, sabemos que manter o mesmo acesso não é seguro. O SenhaFácil seria um modo fácil para as pessoas criarem senhas de modo criativo e interativo.
+
---
+++ index.js
@@ -0,0 +1,37 @@
+// import cipher from './cipher.js';
+
+// document.getElementById("crypt").onclick =
+
+//   function encode(e) {
+//     e.preventDefault();
+
+//     const offset = (document.getElementById("offset").value)
+//     let string = document.getElementById("cod").value
+
+//     let encodeCipher = cipher.encode(offset, string)
+
+//     document.getElementById("encod").value = encodeCipher
+   
+//     const encodeButton = document.getElementById("crypt")
+//     encodeButton.addEventListener("click", encode)
+//   }
+
+  
+// document.getElementById("decrypt").onclick =
+
+//   function decode(e) {
+//     e.preventDefault();
+    
+//     const offset = (document.getElementById("offset").value)
+//     let string = document.getElementById("encod").value
+
+//     let decodeCipher = cipher.decode(offset, string)
+
+//     document.getElementById("encod").value = decodeCipher 
+
+//     const decodeButton = document.getElementById("decrypt")
+//     decodeButton.addEventListener("click", decode)  
+//   }
+
+
+
